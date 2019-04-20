74110402848dc7d62d9424f76d2e5430bf04ad84
GARREN KOLLER <GARKOLLE@uat.edu>
2019-04-20 14:03:57 -0700

    Add project files.

---
+++ Header/Header.h
@@ -0,0 +1,58 @@
+/*/////////////////////////////////////////////////////////////////////////////////////////
+								Program start
+*/////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include <iostream>		//in and out stream
+
+using namespace std;		//standard libary namespace 
+
+
+void welcome() {		//helper function welcome
+
+	
+	cout << "Wellcome The Main Menu" << endl;		//display message
+	cout << "1: " << endl;							//display message
+	cout << "2: " << endl;							//display message
+	cout << "3: " << endl;							//display message
+	cout << "4: " << endl;							//display message
+	cout << "5: " << endl;							//display message
+	cout << "0: " << endl;							//display message
+	cout << "Enter in the number that correlates to the game you want to play." << endl;	//display directions
+	
+
+}
+
+void gameOne() {		//helper function gameOne
+
+	cout << "you have selected game 1" << endl;		//display message
+
+}
+
+void gameTwo() {		//helper function gameTwo
+
+	cout << "you have selected game 2" << endl;		//display message
+
+}
+
+void gameThree() {		//helper function gameThree
+
+	cout << "you have selected game 3" << endl;		//display message
+
+}
+
+void gameFour() {		//helper function gameFour
+
+	cout << "you have selected game 4" << endl;		//display message
+
+}
+
+void gameFive() {		//helper function gameFive
+
+	cout << "you have selected game 5" << endl;		//display message
+
+}
+
+/*/////////////////////////////////////////////////////////////////////////////////////////
+								Program End
+*/////////////////////////////////////////////////////////////////////////////////////////
---
+++ Header/Source.cpp
@@ -0,0 +1,62 @@
+/*/////////////////////////////////////////////////////////////////////////////////////////
+								Program start
+*/////////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>		//in and out stream
+#include <string>		//string
+#include "Header.h"
+
+using namespace std;		//standard libary namespace 
+
+int main() {
+	char playState = 'y';  //intalizing playState = to y
+	int choice;				//intalizing choice
+	while (playState != 'n') {		// while loop 
+	welcome();					//using function welcome from header file
+	cin >> choice;				// players choice
+
+
+		if (choice == 0) {			//if choice = 0
+			cout << "exit" << endl;		//display exit 
+			return 0;				//end program
+		}
+		else if (choice == 1) {		//if choice = 1
+			
+			gameOne();				//run gameOne function from header file
+
+		}
+		else if (choice == 2) {
+			
+			gameTwo();				//run gameTwo function from header file
+
+		}
+		else if (choice == 3) {
+			
+			gameThree();			//run gameThree function from header file
+
+		}
+		else if (choice == 4) {
+		
+			gameFour();				//run gameFour function from header file
+
+		}
+		else if (choice == 5) {
+			
+			gameFive();				//run gameFive function from header file
+			
+		}
+		
+		
+		do {      //do this
+			cout << "Play again? y/n: ";		//display play again
+			cin >> playState;					//give option
+		}
+
+		while (playState != 'y' && playState != 'n');	//while playagain does not = y and n
+	}
+	return 0;		// end program
+}
+
+/*/////////////////////////////////////////////////////////////////////////////////////////
+								Program End
+*/////////////////////////////////////////////////////////////////////////////////////////
